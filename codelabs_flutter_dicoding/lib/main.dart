a405df5b7fe79c7ea99df37ef9fe26ffb4ec1841
dionpouw <aldionsumampouw@gmail.com>
2021-07-25 20:57:50 +0800

    codelabs final 4

--- codelabs_flutter_dicoding/lib/main.dart
+++ codelabs_flutter_dicoding/lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:codelabs_flutter_dicoding/main_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Tempat wisata Bandung',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: DetailScreen());
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: MainScreen(),
+    );
   }
 }
