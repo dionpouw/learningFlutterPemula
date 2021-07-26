b8fe327ecab5305f1764db351294fba3d32280f1
dionpouw <aldionsumampouw@gmail.com>
2021-07-26 09:41:33 +0800

    First phase submission

---
+++ submission_dicoding/lib/screen/mainscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+      title: Text(widget.title),
+    )));
+  }
+}
